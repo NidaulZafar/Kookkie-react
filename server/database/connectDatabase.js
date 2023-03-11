fde703daa55f7283d83aec094c3bd977b33f8dfc
N Zafar <n_zafar_007@hotmail.com>
2023-03-11 19:55:31 +0100

    registration form connected to mongodb atlas

--- server/database/connectDatabase.js
+++ server/database/connectDatabase.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-
+// process.env.MONGODB_URL
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://Settly:Settly123@cluster0.b2f5vli.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MongoDB connected!");
   } catch (err) {
     console.error(err);
