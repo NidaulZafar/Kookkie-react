b7156adb0d4747bdaed46af7d3bcf0046dca5b7e
N Zafar <n_zafar_007@hotmail.com>
2023-03-15 18:00:31 +0100

    refactor

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,18 +3,22 @@ import { Navigate } from "react-router-dom";
 import UserContext from "../contexts/UserContext.js";
 
 const Dashboard = () => {
-  const { userInfo } = useContext(UserContext);
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const handleLogout = () => {
+    setUserInfo(null);
+  };
 
   if (!userInfo) {
     return <Navigate to="/login" replace />;
   }
 
   return (
-    <div className="container">
+    <div className="container dashboard">
       <p className="heading">
         Hi {userInfo.firstName} {userInfo.surname}, welcome to your admin
         account
       </p>
+      <button onClick={handleLogout}>LOGOUT</button>
     </div>
   );
 };
