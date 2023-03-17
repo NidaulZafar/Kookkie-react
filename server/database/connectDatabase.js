4a0908892c58b1e98837e18b6b381dade7094194
N Zafar <n_zafar_007@hotmail.com>
2023-03-17 12:15:48 +0100

    refactor

--- server/database/connectDatabase.js
+++ server/database/connectDatabase.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
-console.log("connectDB", process.env.MONGODB_URL);
 const connectDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
