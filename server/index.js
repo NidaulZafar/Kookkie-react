679408accb449d2e81fc55ac72d2fc9694c3773c
N Zafar <n_zafar_007@hotmail.com>
2023-03-17 11:24:10 +0100

    dependencies

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 dotenv.config();
-console.log("index.js", process.env.SECRET);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
